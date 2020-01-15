53bd527b9e7f496073e9a6d41e48fd88d8208710
xXXIMMMATTHEWXXx <mattparksjr@gmail.com>
2020-01-15 11:54:25 -0500

    Inital Commit

---
+++ src/codes/matthewp/caesar/CaesarCipher.java
@@ -0,0 +1,68 @@
+package codes.matthewp.caesar;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+
+    private static final String possChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1!2@3#4$5%6^7&8*9(0)";
+    private static boolean acceptInput = true;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (acceptInput) {
+            System.out.println("Would you like to encrypt(E), decrypt(D), or quit(Q)?");
+            System.out.print("Please enter your selection: ");
+            String input = scanner.next();
+            scanner.nextLine(); // This must be added for some odd reason, it skips the \n character at EOL
+            if (input.equalsIgnoreCase("E")) {
+                System.out.println("Enter the message you would like to encode:");
+                String str = encode(scanner.nextLine());
+                System.out.println("Encoding message...");
+                System.out.println("Message encoded as: " + str);
+            } else if (input.equalsIgnoreCase("D")) {
+                System.out.println("Enter the message you would like to decode: ");
+                String str = decode(scanner.nextLine());
+                System.out.println("Encoding message...");
+                System.out.println("Message decoded as: " + str);
+            } else if (input.equalsIgnoreCase("Q")) {
+                System.out.println("Quitting....");
+                scanner.close();
+                acceptInput = false;
+            }
+        }
+    }
+
+    private static String encode(String s) {
+        StringBuilder output = new StringBuilder();
+        for (char character : s.toCharArray()) {
+            if (character == ' ') {
+                output.append(" ");
+                continue;
+            }
+            int index = possChars.indexOf(character);
+            index += 3;
+            if (index >= possChars.length()) {
+                index = (index - possChars.length());
+            }
+            output.append(possChars.toCharArray()[index]);
+        }
+        return output.toString();
+    }
+
+    private static String decode(String s) {
+        StringBuilder output = new StringBuilder();
+        for (char character : s.toCharArray()) {
+            if (character == ' ') {
+                output.append(" ");
+                continue;
+            }
+            int index = possChars.indexOf(character);
+            index -= 3;
+            if (index <= 0) {
+                index = (index + possChars.length());
+            }
+            output.append(possChars.toCharArray()[index]);
+        }
+        return output.toString();
+    }
+}
